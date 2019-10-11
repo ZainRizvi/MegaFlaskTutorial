8ebb8bc26dbea5bb5f2d12b5f59f6f8a608aaac2
Zain Rizvi <zainr@google.com>
2019-10-10 19:36:05 -0700

    Add database code

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-wil-never-guess'
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+      'sqlite:///' + os.path.join(basedir, 'app.db')
+  SQLALCHEMY_TRACK_MODIFICATIONS = False # This feature would signal the application every time a change is about to be made in the database.
