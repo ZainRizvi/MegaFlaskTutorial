6261bcba894f1e086935e08a93be938af868d4b6
Ubuntu <zain@megaflask-dev.fqhnijigawfujbyrog5gpm12ee.dx.internal.cloudapp.net>
2019-10-16 15:28:55 +0000

    Add redis

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@ pip install wheel
 pip install -r requirements.txt
 pip install flask-login
 pip install flask-bootstrap
+pip install rq
+# sudo apt-get install redis-server
+# sudo systemctl enable redis-server.service # enable redis service to run on boot
